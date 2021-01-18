8b2961283dc759a225eaa015478224b5279b5cc4
Ara <hello.ara@pm.me>
2021-01-18 22:30:24 +0900

    second commit

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,8 +1,13 @@
 def sort(array)
   sorted_array = []
-  array.size.times do
+  array.size.times
+    num = array.max
     sorted_array.unshift(array.max)
     array.delete_at(array.index(array.max))
   end
   sorted_array
+end
+
+def capital_letter?(letter)
+  letter.upcase == letter 
 end
--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -12,4 +12,23 @@ describe '#sort' do
   it 'sorts 2 elements' do
     expect(sort(['b', 'a'])).to eq ['a', 'b']
   end
+
+  it 'sorts 3 elements' do
+    expect(sort(['c', 'a', 'b'])).to eq ['a', 'b', 'c']
+  end
+
+  it 'sorts 4 elements' do
+    expect(sort(['b', 'd', 'c', 'z'])).to eq ['b', 'c', 'd', 'z']
+  end
+
+  it 'sorts capital letters' do
+    expect(sort(['a', 'B'])).to eq ['a', 'B']
+  end
+end
+
+describe 'capital_letter' do
+  it 'returns true if the letter is a capital letter' do
+    expect(capital_letter?('A')).to be true;
+    expect(capital_letter?('a')).to be false;
+  end
 end
